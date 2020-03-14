22656617fc1f9ca18c973e59b33fbb3253d07488
RachData <rachidderra@gmail.com>
2020-03-14 23:37:41 +0100

    creation de la classe tes pour iterator par groupe

---
+++ Exercice_4_1/src/test/java/fr/uvsq/pglp/Exercice_4_1/afficheGroupeTest.java
@@ -0,0 +1,14 @@
+package fr.uvsq.pglp.Exercice_4_1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class afficheGroupeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
